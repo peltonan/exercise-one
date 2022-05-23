49d1e85fbfac9d1189aa301c77235dea85d35a37
peltonan <peltonen.annette@gmail.com>
2022-05-23 12:34:56 +0300

    Section 4 - Part 52 - Learned useState, setState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+import { useState } from 'react';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated');
+        console.log('Clicked!');
+    };
 
     return (
         <div className="expense-item">
 
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{props.amount}€</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </div>
+
     );
 }
 
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,12 @@
+import './NewExpense.css'
+
+const NewExpense = () => {
+    return
+    <div className="new-expense">
+        <form>
+
+        </form>
+    </div>
+};
+
+export default NewExpense;
