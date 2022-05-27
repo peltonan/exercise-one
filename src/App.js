03b6655a6eff31b245c8a0960ece3509bca37b23
peltonan <peltonen.annette@gmail.com>
2022-05-27 14:48:34 +0300

    Section 4 - Assignment 2 - Events & State Practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import './App.css';
 import ExpenseItem from './components/Expenses/ExpenseItem';
 import NewExpense from './components/NewExpense/NewExpense';
+import ExpensesFilter from './components/Expenses/ExpensesFilter';
 
 import { expenses } from './components/Expenses/ExpensesList'
 
 function App() {
 
+  const addExpenseHandler = expense => {
+    console.log('in App.js');
+    console.log(expense);
+  };
+
   return (
-    <div className="App">
-      <div className="expensesList">
-        <NewExpense />
-        {expenses.map(expense => {
-          return (
-            <ExpenseItem
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          )
-        }
-        )}
+    <div>
+      <div className="App">
+        <div className="expensesList">
+          <NewExpense onAddExpense={addExpenseHandler} />
+          <ExpensesFilter />
+          {expenses.map(expense => {
+            return (
+              <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+              />
+            )
+          }
+          )}
+        </div>
       </div>
     </div>
   );
